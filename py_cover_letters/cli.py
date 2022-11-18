f2b903a912126b84b49c09f6d96e04a438b999c0
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-11-18 08:38:40 -0500

    Command organization.

--- py_cover_letters/cli.py
+++ py_cover_letters/cli.py
@@ -1,25 +1,36 @@
 """Console script for py_cover_letters."""
 
 import click
+from . import __version__ as current_version
 
 
 @click.group()
-def cli():
+@click.version_option(version=current_version)
+def main():
     pass
 
 
-@click.command()
-def initdb():
-    click.echo('Initialized the database')
+@click.command(help='Configure the application.')
+@click.option('--overwrite', is_flag=True, default=False, help="Overwrite the configuration file.")
+def config(overwrite):
+    click.echo('Configuration')
 
 
-@click.command()
-def dropdb():
-    click.echo('Dropped the database')
+@click.command(help='Create cover letters from master Excel file.')
+def create():
+    click.echo('Creating cover letters')
 
 
-cli.add_command(initdb)
-cli.add_command(dropdb)
+@click.command(help='Email cover letters.')
+@click.option('--all', 'email_all', is_flag=True, default=True, help='Email all the unsent cover letters.')
+@click.option('--confirm', is_flag=True, default=True, help='Confirmation before sending the email.')
+def email(email_all: bool, confirm: bool):
+    click.echo('Email cover letters')
+
+
+main.add_command(config)
+main.add_command(create)
+main.add_command(email)
 
 if __name__ == "__main__":
-    cli()  # pragma: no cover
+    main()  # pragma: no cover
