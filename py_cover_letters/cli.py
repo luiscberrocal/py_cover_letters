639c1569f74db9cf4dbc9170e6c04b391b9e950f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-11-19 12:07:18 -0500

    Separated configuration commands.

--- py_cover_letters/cli.py
+++ py_cover_letters/cli.py
@@ -2,8 +2,7 @@
 
 import click
 
-from py_cover_letters.config import ConfigurationManager
-from py_cover_letters.utils import backup_file
+from py_cover_letters.cli_commands.configuration import config
 from . import __version__ as current_version
 
 
@@ -13,32 +12,8 @@ def main():
     pass
 
 
-@click.command(help='Configure the application.')
-@click.option('--overwrite', is_flag=True, default=False, help="Overwrite the configuration file.")
-def config(overwrite):
-    click.echo('Configuration')
-    config_manager = ConfigurationManager()
-    configuration = config_manager.get_sample_config()
-    if config_manager.config_file.exists() and not overwrite:
-        click.echo(f'The configuration already exists ({config_manager.config_file}). Use the --overwrite flag.')
-        return
-    if config_manager.config_file.exists() and overwrite:
-        config_backup_folder = config_manager.config_folder / 'backups'
-        config_backup_folder.mkdir(exist_ok=True)
-        backup_filename = backup_file(config_manager.config_file, config_backup_folder)
-        click.echo(f'Backup of the current config file was made {backup_filename}')
-        configuration = config_manager.get_configuration()
-    new_configuration = configuration.copy()
-    for key, key_conf in configuration.items():
-        click.echo(f'[{key.upper()}]')
-        for sub_key, sub_key_conf in key_conf.items():
-            prompt_text = f'{sub_key.replace("_", " ")}'
-            new_key = click.prompt(prompt_text, default=sub_key_conf)
-            new_configuration[key][sub_key] = new_key
-    config_manager.write_configuration(configuration, over_write=True)
-
-
 @click.command(help='Create cover letters from master Excel file.')
+@click.option('--all', 'create_all', is_flag=True, default=True, help='Create all cover letters.')
 def create():
     click.echo('Creating cover letters')
 
@@ -54,6 +29,5 @@ main.add_command(config)
 main.add_command(create)
 
 main.add_command(email)
-
 if __name__ == "__main__":
     main()  # pragma: no cover
