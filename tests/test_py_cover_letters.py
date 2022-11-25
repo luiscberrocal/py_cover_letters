36bd8ba5b5a6d0b3b8281012c369d782e36a6814
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-11-25 11:01:26 -0500

    All tests running.

--- tests/test_py_cover_letters.py
+++ tests/test_py_cover_letters.py
@@ -11,10 +11,21 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'py-cover-letters' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    result_details = result.output.split('\n')
+    for i, r in enumerate(result_details):
+        print(f'assert result_details[{i}] == \'{r}\'')
+    assert result_details[0] == 'Usage: main [OPTIONS] COMMAND [ARGS]...'
+    assert result_details[1] == ''
+    assert result_details[2] == 'Options:'
+    assert result_details[3] == '  --version  Show the version and exit.'
+    assert result_details[4] == '  --help     Show this message and exit.'
+    assert result_details[5] == ''
+    assert result_details[6] == 'Commands:'
+    assert result_details[7] == '  about   About this application.'
+    assert result_details[8] == '  config  Configure the application.'
+    assert result_details[9] == '  create  Create cover letters from master Excel file.'
+    assert result_details[10] == '  email   Email cover letters.'
+    assert result_details[11] == ''
 
 
 def test_config_show():
