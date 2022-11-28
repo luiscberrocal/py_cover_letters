1b873d07aebeb1fdab8a47e4716e3e720ce6bfa3
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-11-28 12:18:28 -0500

    Changes to keep only Excel.

--- py_cover_letters/db/managers.py
+++ py_cover_letters/db/managers.py
@@ -1,9 +1,15 @@
+import uuid
 from pathlib import Path
 from typing import Protocol, List, Optional, Dict
 
-from py_cover_letters.config import CoverLetter
-from py_cover_letters.constants import COLUMN_MAPPING
+from openpyxl.reader.excel import load_workbook
+from openpyxl.workbook import Workbook
+
+from py_cover_letters.db.models import CoverLetter
+from py_cover_letters.constants import COLUMN_MAPPING, SHEET_NAME
 from py_cover_letters.enums import FilterType
+from py_cover_letters.exceptions import CoverLetterException
+from py_cover_letters.utils import backup_file
 
 
 class CoverLetterManager(Protocol):
@@ -26,13 +32,69 @@ class CoverLetterManager(Protocol):
     def filter(self, filter_type: FilterType) -> List[CoverLetter]:
         ...
 
+
 class ExcelManager:
     def __init__(self, filename: Path, column_mapping: Optional[Dict[int, str]] = None,
-                 sheet_name: str = 'Cover letters'):
+                 sheet_name: str = SHEET_NAME, backup_folder: Optional[Path]= None):
+        if backup_folder is None:
+            raise Exception('xxxxx')
+        else:
+            self.backup_folder = backup_folder
         self.filename = filename
         if column_mapping is None:
             self.column_mapping = COLUMN_MAPPING
         else:
             self.column_mapping = column_mapping
-        self.columns = [col_name for _, col_name in self.column_mapping.items()]
         self.sheet_name = sheet_name
+        self.columns = [col_name for _, col_name in self.column_mapping.items()]
+        self.cover_letters: List[CoverLetter] = list()
+        if not self.filename.exists():
+            self.write_template()
+        else:
+            self.cover_letters = self.list()
+
+    def write_template(self):
+        if self.filename.exists():
+            error_msg = f'Cannot overwrite template {self.filename}'
+            raise CoverLetterException(error_msg)
+
+        wb = Workbook()
+        sheet = wb.create_sheet(self.sheet_name, 0)
+        row = 1
+        col = 1
+        for column_name in self.columns:
+            sheet.cell(row=row, column=col, value=column_name)
+            col += 1
+        row += 1
+
+        wb.save(self.filename)
+
+    def list(self) -> List[CoverLetter]:
+        cover_letters = list()
+        wb = load_workbook(self.filename)
+        sheet = wb[self.sheet_name]
+        last_row = sheet.max_row + 1
+        for row in range(2, last_row):
+            cover_letter_dict = dict()
+            for col, name in self.column_mapping.items():
+                cell_obj = sheet.cell(row=row, column=col)
+                value = cell_obj.value
+                cover_letter_dict[name] = value
+            try:
+                cover_letter = CoverLetter(**cover_letter_dict)
+                cover_letters.append(cover_letter)
+            except Exception as e:
+                error_message = f'Unexpected error on row {row}. Type: {e.__class__.__name__} Error: {e}'
+                raise CoverLetterException(error_message)
+        return cover_letters
+
+    def update_not_saved(self, commit=True) -> List[CoverLetter]:
+        not_saved = [x for x in self.cover_letters if x.id is None]
+        for cover_letter in not_saved:
+            cover_letter.id = uuid.uuid4().int
+            print(cover_letter)
+        if commit:
+            backup_file(self.filename, self.backup_folder)
+            self.filename.unlink()
+            
+        return not_saved
--- tests/db/test_managers.py
+++ tests/db/test_managers.py
@@ -1,40 +1,16 @@
-import inspect
-from pathlib import Path
+import datetime
+import shutil
 
-from py_cover_letters import ConfigurationManager
-from py_cover_letters.db.sqlite import CoverLetterManager
-from py_cover_letters.enums import FilterType
-from tests.factories import CoverLetterFactory
+from py_cover_letters.db.managers import ExcelManager
 
 
-class TestCoverLetterManager:
+def test_update_not_saved(fixtures_folder, output_folder):
+    excel_withou_id = fixtures_folder / 'test_cover_letters_without_ids.xlsx'
+    timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    output_file = output_folder / f'{timestamp}_test_cover_letters_without_ids.xlsx'
+    shutil.copy(excel_withou_id, output_file)
 
-    def test_create(self, testing_database_file):
-        manager = CoverLetterManager(testing_database_file)
+    manager = ExcelManager(output_file, backup_folder=output_folder / 'backups')
 
-        cover_letter = CoverLetterFactory.create(new=True)
-
-        assert cover_letter.id is None
-
-        db_cover_letter = manager.create(cover_letter)
-
-        assert db_cover_letter.id is not None
-        assert cover_letter.id is not None
-
-    def test_filter_deleted(self, cover_letter_manager):
-        deleted = cover_letter_manager.filter(FilterType.COVER_LETTER_NOT_DELETED)
-        all_cover_letters = cover_letter_manager.list()
-        assert len(deleted) == 11
-        assert len(all_cover_letters) == 13
-
-    def test_filter_not_created(self, cover_letter_manager):
-        not_created = cover_letter_manager.filter(FilterType.COVER_LETTER_NOT_CREATED)
-        all_cover_letters = cover_letter_manager.list()
-        assert len(not_created) == 8
-        assert len(all_cover_letters) == 13
-
-
-def test_inspect():
-    sf = Path(inspect.getsourcefile(ConfigurationManager)).parent / 'templates'
-    home_folder = Path.home()
-    assert str(sf) == f'{home_folder}/PycharmProjects/py_cover_letters/py_cover_letters/templates'
+    updated = manager.update_not_saved()
+    assert len(updated) == 50
